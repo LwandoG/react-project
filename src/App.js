3ce20e17a77e5830322f965c9d647ea6cbd5daf2
LwandoG <lwandogqobho@gmail.com>
2020-06-13 23:52:49 +0200

    Moved repos state to context

--- src/App.js
+++ src/App.js
@@ -7,26 +7,15 @@ import  Repos from './components/repos/Repos'
 import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
 import About from './components/pages/About'
-import axios from 'axios'
 import GithubState from './context/github/GithubState'
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
-  const [repos, setRepos] = useState([])
-  const [loading, setLoading] = useState(false)
   const [alert, setAlert] = useState(null)
 
   const showAlert = (msg, type) => {
     setAlert({ msg, type })
     setTimeout(() => setAlert(null), 5000)
-  }
-
-  const getUserRepos = async username => {
-    setLoading(true);
-    const res = await axios.get(`https://api.github.com/user/${username}/repos?per_page=5&sort=created:asc&client_id=
-    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    setRepos(res.data)
-    setLoading(false);
   }
 
     return (
@@ -47,11 +36,7 @@ const App = () => {
              </Fragment>
         )} />
         <Route exact path='/about' component={About}/>
-        <Route exact path='/user/:login' render={props =>(
-           <User {...props}  
-           getUserRepos={getUserRepos}
-            repos={repos} />
-        )}/>
+        <Route exact path='/user/:login' component={User}/>
         </Switch>
         </div>
       </div>
--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 const RepoItem = ({repo}) => {
     return (
         <div>
-            <h3><a href={repo.html_url}>Repo</a></h3>
+            <h3><a href={repo.html_url}>{repo.name}</a></h3>
         </div>
     )
 }
